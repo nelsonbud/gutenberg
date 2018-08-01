f6f5dafa1ffc154a0ad79250a8dc33d16775f464
Daniel Richards <daniel.richards@automattic.com>
2018-08-01 16:08:18 +0800

    Change where keyboard shortcut help modal is rendered
    Move modal so that it's rendered in edit-post/layout component. This ensures
    the modal is available in both the text and visual editor, and the shortcut
    for launching the modal can be used on first load of the editor.

--- edit-post/components/visual-editor/index.js
+++ edit-post/components/visual-editor/index.js
@@ -8,7 +8,6 @@ import {
 	WritingFlow,
 	ObserveTyping,
 	EditorGlobalKeyboardShortcuts,
-	KeyboardShortcutHelpModal,
 	BlockSelectionClearer,
 	MultiSelectScrollIntoView,
 	_BlockSettingsMenuFirstItem,
@@ -24,7 +23,6 @@ function VisualEditor() {
 	return (
 		<BlockSelectionClearer className="edit-post-visual-editor">
 			<EditorGlobalKeyboardShortcuts />
-			<KeyboardShortcutHelpModal />
 			<CopyHandler />
 			<MultiSelectScrollIntoView />
 			<WritingFlow>
